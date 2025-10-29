b1f94f8838a4eb420a4e0f313ab5853cf8fd8437
dgun <hdg20042@gmail.com>
2025-10-29 14:54:22 +0900

    배포 시도

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'backend.navigation',
+    'backend.navigation.apps.NavigationConfig',
     'corsheaders'
 ]
 
